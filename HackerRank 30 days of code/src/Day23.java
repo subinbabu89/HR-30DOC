3391ea7d9496b21eb4f312cda1800f8c699ac79f
Subin Babu <subin.babu@mavs.uta.edu>
2016-08-29 04:27:47 -0500

    Day 23 - Fixed Issues with Node

--- HackerRank 30 days of code/src/Day23.java
+++ HackerRank 30 days of code/src/Day23.java
@@ -2,54 +2,58 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-class Node{
-    Node left,right;
-    int data;
-    Node(int data){
-        this.data=data;
-        left=right=null;
-    }
+class Day23Node {
+	Day23Node left, right;
+	int data;
+
+	Day23Node(int data) {
+		this.data = data;
+		left = right = null;
+	}
 }
+
 public class Day23 {
-	static void levelOrder(Node root){
-	      Queue<Node> queue = new LinkedList();
-	    queue.add(root);
-	    
-	    while(!queue.isEmpty()){
-	        Node current = queue.remove();
-	        System.out.print(current.data+" ");
-	        if (current.left!=null) queue.add(current.left);
-	        if (current.right!=null) queue.add(current.right);
-	    }
-	      
-	    }
-	
-	public static Node insert(Node root,int data){
-        if(root==null){
-            return new Node(data);
-        }
-        else{
-            Node cur;
-            if(data<=root.data){
-                cur=insert(root.left,data);
-                root.left=cur;
-            }
-            else{
-                cur=insert(root.right,data);
-                root.right=cur;
-            }
-            return root;
-        }
-    }
-    public static void main(String args[]){
-            Scanner sc=new Scanner(System.in);
-            int T=sc.nextInt();
-            Node root=null;
-            while(T-->0){
-                int data=sc.nextInt();
-                root=insert(root,data);
-            }
-            levelOrder(root);
-        }	
+	static void levelOrder(Day23Node root) {
+		Queue<Day23Node> queue = new LinkedList<>();
+		queue.add(root);
+
+		while (!queue.isEmpty()) {
+			Day23Node current = queue.remove();
+			System.out.print(current.data + " ");
+			if (current.left != null)
+				queue.add(current.left);
+			if (current.right != null)
+				queue.add(current.right);
+		}
+
+	}
+
+	public static Day23Node insert(Day23Node root, int data) {
+		if (root == null) {
+			return new Day23Node(data);
+		} else {
+			Day23Node cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
+	}
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		Day23Node root = null;
+		while (T-- > 0) {
+			int data = sc.nextInt();
+			root = insert(root, data);
+		}
+		levelOrder(root);
+		sc.close();
+	}
 
 }
