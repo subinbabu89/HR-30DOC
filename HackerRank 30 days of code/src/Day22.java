885f94327f1b118b886c491292e9f62dd18ca504
Subin Babu <subin.babu@mavs.uta.edu>
2016-08-29 04:26:42 -0500

    Day22 - Fixed Issues with Nodes

--- HackerRank 30 days of code/src/Day22.java
+++ HackerRank 30 days of code/src/Day22.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-class Node {
-	Node left, right;
+class Day22Node {
+	Day22Node left, right;
 	int data;
 
-	Node(int data) {
+	Day22Node(int data) {
 		this.data = data;
 		left = right = null;
 	}
 }
 
 public class Day22 {
-	public static int getHeight(Node root) {
+	public static int getHeight(Day22Node root) {
 		int heightLeft = 0;
 		int heightRight = 0;
 
@@ -25,11 +25,11 @@ public class Day22 {
 		return (heightLeft > heightRight ? heightLeft : heightRight);
 	}
 
-	public static Node insert(Node root, int data) {
+	public static Day22Node insert(Day22Node root, int data) {
 		if (root == null) {
-			return new Node(data);
+			return new Day22Node(data);
 		} else {
-			Node cur;
+			Day22Node cur;
 			if (data <= root.data) {
 				cur = insert(root.left, data);
 				root.left = cur;
@@ -44,13 +44,14 @@ public class Day22 {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		Node root = null;
+		Day22Node root = null;
 		while (T-- > 0) {
 			int data = sc.nextInt();
 			root = insert(root, data);
 		}
 		int height = getHeight(root);
 		System.out.println(height);
+		sc.close();
 	}
 
 }
